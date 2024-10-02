b73ff6cd5cc9ab29585dfaba0fab6875c3b29cae
Victor Garritano <victor@getupgraded.com>
2024-10-02 20:47:03 +0200

    Elementos dinamicos anadidos con bucle

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
 
-    console.log("hola");
-    const body = document.querySelector("body")
-    
-    let div = document.createElement("div");
-    let banner = document.createElement("banner");
-    let divfotos = document.createElement("divfotos");
-    let selector = document.createElement("select");
+console.log("hola");
 
-    
+let imagenes = [  // Array para introducir imagenes
+'viajes/viajes-1.jpg', 
+'viajes/viajes-2.jpg', 
+'viajes/viajes-3.jpg',
 
-    body.appendChild(div);
-    div.appendChild(banner);
-    banner.appendChild(divfotos);
-
-
-    var DOM_img_banner = document.createElement("img");
+];
+let textoParrafo = [
+    "Este es el texto del párrafo 1", 
+    "Este es el texto del párrafo 2", 
+    "Este es el texto del párrafo 3"
+];
+let destinos = [
+    'Burgos',
+    'Madrid',
+    'Mallorca',
+    'La Palma',
+    'Tenerife'
+    ];
+var DOM_img_banner = document.createElement("img");
     DOM_img_banner.src = "./banner/1.jpg";
-    var fotoViaje1 = document.createElement("img");
-    fotoViaje1.src = "./viajes/viajes-1.jpg";
-    var fotoViaje2 = document.createElement("img");
-    fotoViaje2.src = "./viajes/viajes-2.jpg";
-    var fotoViaje3 = document.createElement("img");
-    fotoViaje3.src = "./viajes/viajes-3.jpg";
-
-    var options = document.createElement("option");
-    var options2 = document.createElement("option");
-    
-    let txt = document.createTextNode("Burgos");
-    let txt2 = document.createTextNode("Madrid")
-   
     document.querySelector("banner").appendChild(DOM_img_banner);
-    document.querySelector("divfotos").appendChild(fotoViaje1);
-    document.querySelector("divfotos").appendChild(fotoViaje2);
-    document.querySelector("divfotos").appendChild(fotoViaje3);
-    document.querySelector("banner2").appendChild(selector);
-    document.querySelector("select").appendChild(options);
-    document.querySelector("select").appendChild(options2);
-    options.appendChild(txt);
-    options2.appendChild(txt2);
+for (let i=0; i< 3; i++){
+    const img = document.createElement('img');
+    img.src = imagenes[i];
+    document.querySelector("divfotos").appendChild(img); 
+    img.id = `img-${i+1}`;
+    const parrafos = document.createElement('h4');
+    document.querySelector("divtexto").appendChild(parrafos);
+    parrafos.id = `h4-${i+1}`;
+    const textop= document.createTextNode(textoParrafo[i]);
+    document.querySelector("divtexto").appendChild(textop);
     
+};
 
 
-// document.querySelector("button").addEventListener("click", function () {
-// });
+//selector
+
+
+let selector = document.createElement("select");
+document.querySelector("banner2").appendChild(selector);
+for (let j = 0; j< destinos.length; j++) {
+    
+    let options = document.createElement("option");
+    document.querySelector("select").appendChild(options);
+    let txt = document.createTextNode(destinos[j]);
+    options.appendChild(txt);
+};
